215b23b9ba5572475f95da417bb92b7a0f74244a
SwitchNerd <avikamediwa@gmail.com>
2023-09-15 21:31:55 +0530

    future.me.will.finish.p2

--- CalorieCounting/main.py
+++ CalorieCounting/main.py
@@ -1,6 +1,8 @@
 test = []
 _ = 0
 max = 0
+max1 = 0
+max2 = 0
 with open('datbase.txt', 'r') as data:
    test = data.readlines()
    
@@ -10,5 +12,10 @@ with open('datbase.txt', 'r') as data:
     else:
        if max < _:
            max = _
+       elif max1 < _:
+           max1 = _
+       elif max2 < _:
+           max2 = _
        _ = 0
-   print(max)
+    #u are doing part 2, the problem with aboove code is that if the 2nd largest numbers gets registerd at max before the largest, then it is lost after the largest gets registered and so on
+   print(max,max1,max2)
